bfdba4d57dc7721bc229508a4468152216172268
muammar88 <muammarkadafi88@gmail.com>
2025-09-03 11:02:06 +0700

    x

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+import { useHead } from '@vueuse/head'
+
+useHead({
+  title: 'Aplikasi Muzakki & Mustahik Kabupaten Aceh Tengah',
+  link: [ { rel: 'icon', type: 'image/png', href: 'public/favicon.png' } ],
+  meta: [ { name: 'AMRA', content: 'Aplikasi manajemen travel Haji dan Umrah' } ]
+});
+
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped></style>
